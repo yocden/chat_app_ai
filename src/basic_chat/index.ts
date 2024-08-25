5f82c7867dd37ff71a01b17ccd286e9445238fdb
yocden <yuji.ocden@gmail.com>
2024-08-25 23:28:49 +0800

    first commit

---
+++ src/basic_chat/index.ts
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+const openai = new OpenAI();
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+  { role: "system", content: " You are a helpful chatbot" },
+];
+
+async function createChatCompletion() {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: context,
+  });
+  const responseMessage = response.choices[0].message;
+  context.push({
+    role: "assistant",
+    content: responseMessage.content,
+  });
+  console.log(
+    `${response.choices[0].message.role}:${response.choices[0].message.content}`
+  );
+}
+
+process.stdin.addListener("data", async function (input) {
+  const userInput = input.toString().trim();
+  context.push({
+    role: "user",
+    content: userInput,
+  });
+  await createChatCompletion();
+});
