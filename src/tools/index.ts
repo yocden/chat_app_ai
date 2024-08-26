d7f2d14f350ed2194afc893365cd469d8f0112bf
yocden <yuji.ocden@gmail.com>
2024-08-26 10:25:26 +0800

    update index.ts of tools as for the activity of sec4.23 first tool call

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -4,6 +4,10 @@ import OpenAI from "openai";
 dotenv.config();
 const openai = new OpenAI();
 
+function getTimeOfDay() {
+  return "5:45";
+}
+
 async function callOpenAIWithTools() {
   const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
     {
@@ -15,13 +19,42 @@ async function callOpenAIWithTools() {
       role: "user",
       content: "What is the current date and time in New York City?",
     },
-  ]
+  ];
   //configure chat tools(call openAI)
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: context
-  })
-  console.log(response.choices[0].message.content)
+    messages: context,
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "getTimeOfDay",
+          description: "Get the time of day",
+        },
+      },
+    ],
+    tool_choice: "auto", // engine will decide which tool to use
+  });
+
+  //decide if tool call is required
+  const toolCall = response.choices[0].message.tool_calls?.[0];
+  if (response.choices[0].finish_reason === "tool_calls" && toolCall) {
+    const toolName = toolCall.function.name;
+    if (toolName === "getTimeOfDay") {
+      const toolResponse = getTimeOfDay();
+      context.push(response.choices[0].message);
+      context.push({
+        role: "tool",
+        content: toolResponse,
+        tool_call_id: toolCall.id,
+      });
+    }
+  }
+  const secondResponse = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: context,
+  });
+  console.log(secondResponse.choices[0].message.content);
 }
 
 callOpenAIWithTools();
